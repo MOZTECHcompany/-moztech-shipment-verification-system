db5e19fee399f93a3d01bbb4c14d63480420e865
MOZTECHcompany <moztecheason@gmail.com>
2025-06-07 22:35:06 +0000

    refactor: Link CSS directly in HTML to bypass Vite processing issue

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,11 @@
 // src/main.jsx
-
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
-// 【關鍵修改】將 App 的 import 移到 CSS import 的下面
-// 確保 CSS 檔案被優先處理
-import './index.css'; 
-import App from './App.jsx';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+// import './index.css' // <--- 我們把這一行註解掉或刪除
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-);
+)
