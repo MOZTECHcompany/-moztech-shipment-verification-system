1bca373cf91cd866cf18f60782bba546961a309e
MOZTECHcompany <moztecheason@gmail.com>
2025-06-07 22:14:49 +0000

    fix: Ensure CSS is imported before App in main.jsx。

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// src/main.jsx
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+// 【關鍵修改】將 App 的 import 移到 CSS import 的下面
+// 確保 CSS 檔案被優先處理
+import './index.css'; 
+import App from './App.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>,
+);
