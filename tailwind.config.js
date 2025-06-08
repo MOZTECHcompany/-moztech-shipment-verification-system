d33a15ba2ef62fa0310a6e21bcc97ec6bc53ac6d
MOZTECHcompany <moztecheason@gmail.com>
2025-06-08 00:36:44 +0000

    feat: Change font to Noto Sans TC for better readability。

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from 'tailwindcss/defaultTheme' // 【新增】引入預設主題
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      // 【關鍵修改】將 Noto Sans TC 加入到預設的 font-sans 列表的最前面
+      fontFamily: {
+        sans: ['"Noto Sans TC"', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 }
