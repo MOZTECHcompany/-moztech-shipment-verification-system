a5c41a0beed0edb4a6779dfa339e8216c80c2734
MOZTECHcompany <moztecheason@gmail.com>
2025-06-08 17:09:08 +0000

    N3

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
+// src/main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' // <--- 確保這一行存在
+import './index.css' // <--- 這一行是載入所有樣式的關鍵！
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
